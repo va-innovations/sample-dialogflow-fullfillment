6069f6a916b0b17ad3b3f37ba7f66ce0c5116431
va-innovations <92372430+va-innovations@users.noreply.github.com>
2021-10-12 14:35:45 +0300

    Comments

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
+// Import dependencies
 const express = require('express');
 const cors = require('cors');
 const {WebhookClient} = require('dialogflow-fulfillment');
 const {getWeeklySchedule} = require('./comms.js');
-
-require("dotenv").config();
-
-const PORT = process.env.PORT;
-
+// Set up Express app
 const app = express();
+//Apply middleware
+require("dotenv").config();
 app.use(cors());
 app.use(express.json());
+//Define port number
+const PORT = process.env.PORT;
 
 
+
+//Define Dialogflow fulfillment function
 const dialogflowFulfillment = (req,res) => {
+    
+    // Create an agent which will process requests and responses 
     const agent = new WebhookClient({request:req, response:res});
-
+    
+    //Handler function for getDaySchedule intent
     const getDayScheduleHandler = async (agent) => {
         const dayOfWeek = req.body.queryResult.parameters.dayOfWeek;
         console.log(`Day of week: ${dayOfWeek}`)
@@ -27,16 +33,16 @@ const dialogflowFulfillment = (req,res) => {
         }
         
     }
-
+    //Create a new intent map to map the inents with their handler functions
     let intentMap = new Map();
     intentMap.set('DayClassSchedule',getDayScheduleHandler);
     agent.handleRequest(intentMap);
 }
-
+//Define a route for Dialogflow to perform requests to
 app.post('/api', (req,res) => {
     dialogflowFulfillment(req,res);
 })
-
+//Launch application
 app.listen(PORT, () => {
     console.log(`The UniBot API is listening on port: ${PORT}`)
 })
